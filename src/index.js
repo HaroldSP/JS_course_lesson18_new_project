ec1d34be11002e9fbab56f55a5ecd70446358dae
HaroldSP <george.st.petersburg@gmail.com>
2023-02-23 21:44:57 +0300

    les20 (extra task): smooth scroll added

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@
 import timer from './modules/timer.js';
 import menu from './modules/menu.js';
 import modal from './modules/modal';
+import scroll from './modules/scroll';
 
 timer('24 February 2023');
 // timer('20 February 2023');
 menu();
 modal();
+scroll();
