0896bd64de20e8dd07213f17f1ce5887e39f4e2c
HaroldSP <george.st.petersburg@gmail.com>
2023-02-24 18:40:45 +0300

    les21 (regexp): first task (1/2)

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ import timer from './modules/timer.js';
 import menu from './modules/menu.js';
 import modal from './modules/modal';
 import scroll from './modules/scroll';
+import calc from './modules/calc';
 
 timer('24 February 2023');
-// timer('20 February 2023');
 menu();
 modal();
 scroll();
+calc();
